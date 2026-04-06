49d63b8690e304c4ecba749298c81ae5f231933e
Ayan Huda <ayanhudda05@gmail.com>
2026-04-06 15:58:51 -0500

    Update arp.py
    Added a better format to include the manufacturing/hostname of the device

--- discovery/arp.py
+++ discovery/arp.py
@@ -1,9 +1,10 @@
-from scapy.all import ARP, Ether, srp 
+from scapy.all import ARP, Ether, srp
+import socket
+from manuf import manuf
 
-target_ip = '144.167.112.108' #target ip 
+target_ip = '144.167.112.5/24' #target ip 
 
 arp = ARP(pdst=target_ip) #create arp packet
-
 ether = Ether(dst='ff:ff:ff:ff:ff:ff') #create ether broadcast packet, mac address indicates broadcasting
 packet = ether/arp #stack them 
 
@@ -13,11 +14,18 @@ result = srp(packet, timeout=3)[0] #send and recveive packet at layer 2, set a t
 clients=[]
 
 for sent, received in result: 
-    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+    ip = received.psrc
+    mac = received.hwsrc
+    
+    p = manuf.MacParser() #create manuf parser object
+    vendor = p.get_manuf(mac) #get the vendor of the device using the mac address
+    hostname = socket.getfqdn(ip) #get the hostname of the device using the ip address
+
+    clients.append({'ip': ip, 'mac': mac, 'hostname': hostname, 'vendor': vendor})
     
 #print clients
 print(f'Available devices on the network')
-print(f'IP' + ' '*18+'mac')
+print(f'IP' + ' '*18+'mac' + ' '*18 + 'hostname' + ' '*18 + 'vendor')
 
 for client in clients:
-    print("{:16}  {}".format(client['ip'], client['mac']))
+    print("{:16}  {}".format(client['ip'], client['mac'], client['hostname']))
